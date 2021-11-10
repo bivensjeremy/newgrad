3b821e787a28bec341701218a9416922ef50a3a3
Jeremy <bivens.jeremy@gmail.com>
2021-11-10 10:50:53 -0500

    update URL for Nextauth

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000' : 'https://newgrad.bivensblueprint.com'
+export const server = dev ? 'http://localhost:3000' : 'https://newgrad.vercel.app/'
